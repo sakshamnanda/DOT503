201e44a4ca3f334db0986f941937ba5b9f080da9
Saksham Nanda <sakshamnanda@gmail.com>
2022-04-13 21:45:01 +1000

    Update Feature Z

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     let submitBtn = document.getElementById("submit");
     submitBtn.addEventListener("click", function() {
-        if (document.getElementById("answer").value == 4) {
+        if (document.getElementById("answer").value == 2) {
             alert("Correct answer!");
         } else {
             alert("Wrong answer!");
