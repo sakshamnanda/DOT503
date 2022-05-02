f01f96b5e98b9d35510a3263d043823ec193abbf
dot503 <sakshamnanda@gmail.com>
2022-05-02 14:13:34 +0530

    Update sum.test.js
    fix unit test

--- sum.test.js
+++ sum.test.js
@@ -12,10 +12,10 @@ test('Test 3: adds 3 + 6 to equal 9', () => {
     expect(sum(3, 6)).toBe(9);
 });
 
-test('Test 4: adds 1 + 1 to equal 11', () => {
-    expect(sum(1, 1)).toBe(11);
+test('Test 4: adds 1 + 1 to equal 2', () => {
+    expect(sum(1, 1)).toBe(2);
 });
 
-test('Test 5: adds 2 + 2 to equal 2', () => {
-    expect(sum(2, 2)).toBe(2);
+test('Test 5: adds 2 + 2 to equal 4', () => {
+    expect(sum(2, 2)).toBe(4);
 });
